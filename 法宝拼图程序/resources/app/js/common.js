629741bdbd926ad5a2388d059c8c404bb92771c1
baron <450175465@qq.com>
2018-11-29 18:27:19 +0800

    a

--- 法宝拼图程序/resources/app/js/common.js
+++ 法宝拼图程序/resources/app/js/common.js
@@ -42,6 +42,34 @@ $(function()
 
     require('electron').webFrame.setZoomLevelLimits(1,1);
 
+   /* let  fs = require('fs');
+    let  join = require('path').join;
+
+    function findSync(startPath) {
+        let result=[];
+        function finder(path) {
+            let files=fs.readdirSync(path);
+            files.forEach((val,index) => {
+                let fPath=join(path,val);
+            let stats=fs.statSync(fPath);
+            if(stats.isDirectory()) finder(fPath);
+            if(stats.isFile()) result.push(fPath);
+        });
+
+        }
+        finder(startPath);
+        return result;
+    }
+    let fileNames=findSync('./resources/app/img/3开始游戏页面/素材/掉落的玩具');
+    var list=[];
+    for(var i=0;i<fileNames.length;i++)
+    {
+        var a=fileNames[i].split("\\")[6];
+        list.push(a.split(".")[0]);
+    }
+    console.log(JSON.stringify(list));*/
+
+
 });
 
 //$("body").append("<div onclick='window.location.reload();' style='width:200px;height:200px;position: absolute;right:0;bottom:0;background: #000;z-index:10000;'></div>")
